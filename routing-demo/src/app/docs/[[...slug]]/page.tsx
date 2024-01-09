348962588dbb5402d09770b1c3ffeef6adf4a52b
Puneet Vaishnav <puneet@coreteams.us>
2024-01-09 23:53:57 +0530

    improved slug declaration

--- routing-demo/src/app/docs/[[...slug]]/page.tsx
+++ routing-demo/src/app/docs/[[...slug]]/page.tsx
@@ -1,10 +1,13 @@
 const Docs = ({ params }: { params: { slug: string[]; }; }) => {
     console.log(params);
     const slugLength = params?.slug?.length;
+    const slugAt0 = params?.slug[0];
+    const slugAt1 = params?.slug[0];
+    const slugAt2 = params?.slug[0];
     switch (slugLength) {
-        case 1: return <h1>Viewing Docs for feature {params?.slug[0]}</h1>
-        case 2: return <h1>Viewing Docs for feature {params?.slug[0]} and concept {params?.slug[1]}</h1>
-        case 3: return <h1>Viewing Docs for feature {params?.slug[0]} and concept {params?.slug[1]} and example {params?.slug[2]}</h1>
+        case 1: return <h1>Viewing Docs for feature {slugAt0}</h1>
+        case 2: return <h1>Viewing Docs for feature {slugAt0} and concept {slugAt1}</h1>
+        case 3: return <h1>Viewing Docs for feature {slugAt0} and concept {slugAt1} and example {slugAt2}</h1>
         default: return <h1>Docs Home Page</h1>
     }
 }
