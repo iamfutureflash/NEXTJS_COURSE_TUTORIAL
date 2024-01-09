2c5f020a95ff529d087166fe756e34e535f200e9
Puneet Vaishnav <puneet@coreteams.us>
2024-01-09 23:54:44 +0530

    updated index for slugs

--- routing-demo/src/app/docs/[[...slug]]/page.tsx
+++ routing-demo/src/app/docs/[[...slug]]/page.tsx
@@ -2,8 +2,8 @@ const Docs = ({ params }: { params: { slug: string[]; }; }) => {
     console.log(params);
     const slugLength = params?.slug?.length;
     const slugAt0 = params?.slug[0];
-    const slugAt1 = params?.slug[0];
-    const slugAt2 = params?.slug[0];
+    const slugAt1 = params?.slug[1];
+    const slugAt2 = params?.slug[2];
     switch (slugLength) {
         case 1: return <h1>Viewing Docs for feature {slugAt0}</h1>
         case 2: return <h1>Viewing Docs for feature {slugAt0} and concept {slugAt1}</h1>
